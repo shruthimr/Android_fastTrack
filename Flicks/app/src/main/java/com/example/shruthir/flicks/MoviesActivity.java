8214addb21cf8eacaf561940fda2b7b2c0806fb2
Shruthi Ramesh <shruthir@yahoo-inc.com>
2016-11-11 21:55:57 -0800

    pull to refresh

--- Flicks/app/src/main/java/com/example/shruthir/flicks/MoviesActivity.java
+++ Flicks/app/src/main/java/com/example/shruthir/flicks/MoviesActivity.java
@@ -1,6 +1,7 @@
 package com.example.shruthir.flicks;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ListView;
@@ -23,17 +24,42 @@ public class MoviesActivity extends AppCompatActivity {
     ArrayList<Movie> movies;
     MovieArrayAdapter movieArrayAdapter;
     ListView lvItems;
+    private SwipeRefreshLayout swipeContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
 
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+
+        // Setup refresh listener which triggers new data loading
+
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                fetchTimelineAsync();
+            }
+        });
+
+        // Configure the refreshing colors
+
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+
         lvItems = (ListView)    findViewById(R.id.lvMovies);
         movies = new ArrayList<>();
         movieArrayAdapter = new MovieArrayAdapter(this, movies);
         lvItems.setAdapter(movieArrayAdapter);
+        fetchTimelineAsync();
 
+    }
+
+    public void fetchTimelineAsync()
+    {
         String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
         AsyncHttpClient client = new AsyncHttpClient ();
         client.get (url, new JsonHttpResponseHandler(){
@@ -46,14 +72,13 @@ public class MoviesActivity extends AppCompatActivity {
                     Log.d("Results = %s", moviesResults.toString());
                     movies.addAll(Movie.parseMovieArray(moviesResults));
                     movieArrayAdapter.notifyDataSetChanged();
+                    swipeContainer.setRefreshing(false);
                 }
                 catch (JSONException e)
                 {
                     e.printStackTrace();
                 }
-
             }
-
             public void onFailure(int statusCode, Header[] headers, String responseString) {
                 Log.d("Response = %s" ,responseString);
             }
--- Flicks/readme.md
+++ Flicks/readme.md
@@ -9,6 +9,7 @@ Time spent: 5 hours spent in total
 The following **required** functionality is completed:
 
 * [x] User can **scroll through current movies** from the Movie Database API
+<<<<<<< Updated upstream
 * [x ] For each movie displayed, user can see the following details:
   * [x] Title, Poster Image, Overview (Portrait mode)
   * [x] Title, Backdrop Image, Overview (Landscape mode)
@@ -43,7 +44,7 @@ The following **additional** features are implemented:
 
 Here's a walkthrough of implemented user stories:
 
-<img src='http://i.imgur.com/link/to/your/gif/file.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+<img src='http://i.imgur.com/CYh7s9U.gifv' title='Video Walkthrough' width='' alt='Video Walkthrough' />
 
 GIF created with [LiceCap](http://www.cockos.com/licecap/).
 
